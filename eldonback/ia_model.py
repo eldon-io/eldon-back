49a0d2a6c21b1ab09b161fa86453adcfc830a66e
Julien Lambert <julien.lambert@edu.devinci.fr>
2023-04-16 15:25:43 -0400

    Update ia_model.py

--- eldonback/ia_model.py
+++ eldonback/ia_model.py
@@ -19,28 +19,15 @@ nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 
 
-### Traitement de la prédiction renvoyé par l'algorithme gpt-3
-def traitement_prediction(prediction):
-  msg = prediction.lower().split()
-  response = 0
-
-  if 'negative' in msg or 'negative.' in msg:
-    response = 1
-  
-  # pour vérifier 
-  # print(msg, response)
-
-  return response
-
 ### Prédiction de la nature du message
 def algorithm_gpt3(x_to_classify):
     openai.api_key = SECRET_KEY_OPEN_AI
     
-    response = openai.Completion.create(
-    model="text-babbage-001",
-    prompt="Classify the following text with the words positive or negative: \n"+ x_to_classify +"\nCategory:")
-    
-    msg = response.choices[0].text
-    y_predicted = traitement_prediction(msg)
+    response = openai.Moderation.create(input=x_to_classify)
+    response = response['results'][0]['flagged']
+    try:
+      y_predicted = 1 if response==True else 0
+    except Exception as e:
+      print(str(e))
     
     return y_predicted
